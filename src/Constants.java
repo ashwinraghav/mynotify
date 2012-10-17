f5332ce4b375f1a02c44d5ec529689eefe1ff5c5
Ashwin Raghav <ashwinraghav@gmail.com>
2012-10-17 13:26:35 -0400

    #Ashwin, model producer consumer queue. Have added a constants File. This has the config stuff

---
+++ src/Constants.java
@@ -0,0 +1,4 @@
+
+public class Constants {
+	public static final String host = "elmer.cs.virginia.edu";
+}
--- src/RabbitMQProducer.java
+++ src/RabbitMQProducer.java
@@ -1,23 +1,21 @@
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.*;
 
 public class RabbitMQProducer {
-	public static void main(String a[])throws Exception {
+	public static void main(String a[]) throws Exception {
 		ConnectionFactory factory = new ConnectionFactory();
-		 factory.setUsername("guest");
-		 factory.setPassword("guest");
-		 factory.setVirtualHost("/");
-		 factory.setHost("elmer.cs.virginia.edu");
-		 factory.setPort(5672);
-		 Connection conn = factory.newConnection();
-		      Channel channel = conn.createChannel();
-		      String exchangeName = "myExchange";
-		      String routingKey = "testRoute";
-		      byte[] messageBodyBytes = "Hello, world!".getBytes();
-		      channel.basicPublish(exchangeName, routingKey
-		,MessageProperties.PERSISTENT_TEXT_PLAIN, messageBodyBytes) ;
-		      channel.close();
-		      conn.close();
+		factory.setUsername("guest");
+		factory.setPassword("guest");
+		factory.setVirtualHost("/");
+		factory.setHost(Constants.host);
+		factory.setPort(5672);
+		Connection conn = factory.newConnection();
+		Channel channel = conn.createChannel();
+		String exchangeName = "myExchange";
+		String routingKey = "testRoute";
+		byte[] messageBodyBytes = "Hello, world!".getBytes();
+		channel.basicPublish(exchangeName, routingKey,
+				MessageProperties.PERSISTENT_TEXT_PLAIN, messageBodyBytes);
+		channel.close();
+		conn.close();
 	}
 }
