bf3fbf4a3816f53742f27e99861d773f6459e9ee
Tommy Tracy II <tjt7a@virginia.edu>
2012-10-20 00:16:03 -0400

    Use command-line argument as path

--- src/watch/SubscriptionReceiver.java
+++ src/watch/SubscriptionReceiver.java
@@ -5,13 +5,19 @@ import java.io.IOException;
 public class SubscriptionReceiver {
 	final static int poolSize = 50;
 
-	public static void main(String a[]) {
+	public static void main(String args[]) {
+		
+		//If no argument is passed; use testing default
+		if(args.length == 0){
+			args[0] = new String();
+			args[0] = "/localtmp/dump";
+		}
+		
 		Pool pool = new Pool(poolSize);
+		
 		try {
-
 			for (int i = 0; i <= 1000; i++) {
-				//System.out.println("/localtmp/dump/" + i);
-				pool.watch("/localtmp/dump/" + i, false);
+				pool.watch(args[0] + i, false);		
 			}
 			// pool.stopWatching("/if8/am2qa/Desktop");
 		} catch (IOException e1) {
