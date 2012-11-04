7ff6e2f5e79648c68b55ac4125969fdd79eba2fa
Tommy Tracy II <tjt7a@virginia.edu>
2012-11-04 10:36:00 -0500

    Added code to distinguish between local and NFS directories. Tested with MAC; needs to be tested with Linux. Right now all the program does is print if the file is local or NFS mounted. We will need to add code to do local watching if the file is local.

--- src/watch/ClientSubscriber.java
+++ src/watch/ClientSubscriber.java
@@ -3,15 +3,22 @@ package watch;
 import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
 import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
 import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.WatchEvent;
 import java.nio.file.WatchEvent.Kind;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.QueueingConsumer;
 
 public class ClientSubscriber {
 
-	public static void subscribe(String dirName, boolean durable,
+	public static void nfssubscribe(String dirName, boolean durable,
 			WatchEvent.Kind<?>... subscriptionTypes) {
 
 		try {
@@ -61,8 +68,74 @@ public class ClientSubscriber {
 
 	public static void main(String[] a) {
 		boolean durable = false;
-		subscribe("/localtmp/dump/0/1", durable, ENTRY_CREATE, ENTRY_DELETE,
+		boolean isNFS = false;
+		String directory = "/localtmp/dump/0/1";
+		ArrayList<String> mountedDirectories = new ArrayList<String>();
+		
+		//Allow user to pass name of directory as command line argument
+		if(a.length == 1)
+			directory = a[0];
+		
+		//Check if directory is local or mounted via NFS
+		//Unfortunately, ever OS is different, so we're going to need to parse `mount`s differently
+		//The main difference between mac and linux is the placement of the mount type
+		String os = System.getProperty("os.name").toLowerCase();
+		String searchString = null;
+		
+		if(os.contains("mac")){
+			System.out.println("You're running on a mac!");
+			searchString = "(nfs";
+		}
+		else if(os.contains("nix")){
+			System.out.println("You're running on Linux/Unix Machine!");
+			searchString = "type nfs";
+		}
+			
+		String line = "";
+		
+		try{
+			Runtime r = Runtime.getRuntime();
+			Process p = r.exec("mount");
+			p.waitFor();
+			BufferedReader b = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			ArrayList<String> mount_info = new ArrayList<String>();
+			
+			while((line = b.readLine()) != null){
+				mount_info.add(line);
+			}
+
+			for(int i = 0; i < mount_info.size(); i++){
+				if(mount_info.get(i).contains(searchString)){
+				  ArrayList<String> splitLine = new ArrayList<String>(Arrays.asList(mount_info.get(i).split(" ")));
+				  int indexOfPath = splitLine.indexOf("on");
+				  String mountPoint = splitLine.get(++indexOfPath);
+				  System.out.println("Found a mounted NFS directory!: "+mountPoint);
+				  mountedDirectories.add(mountPoint);
+				}
+			}
+
+		}catch(Exception e){
+			System.out.println("Error running `mount` command");
+			e.printStackTrace();
+			System.exit(-1);
+		}
+		
+		for(int i = 0; i < mountedDirectories.size(); i++){
+			if(directory.startsWith(mountedDirectories.get(i))){
+				isNFS = true;
+				break;
+			}
+		}
+		
+		if(isNFS)
+			System.out.println("This directory is Remotely Accessed!");
+		else
+			System.out.println("This directory is locally accessible");
+		
+		nfssubscribe(directory, durable, ENTRY_CREATE, ENTRY_DELETE,
 				ENTRY_MODIFY);
+		
+
 	}
 
 }
