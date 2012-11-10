ae6438a6b409620826de588643f1fb76bf0bade3
Tommy Tracy II <tjt7a@virginia.edu>
2012-11-10 17:51:41 -0500

    Added comments and cleaned up

--- src/watch/BurstController.java
+++ src/watch/BurstController.java
@@ -1,35 +1,64 @@
 package watch;
 
-import java.nio.file.Path;
 import java.nio.file.WatchKey;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class BurstController {
-	private ConcurrentHashMap<WatchKey, NotificationTimeStamp> notificationTimeStamps;
-	private final static long thresholdCount = 10;
-	private final static long thresholdTime = 5 * 1000; // milliseconds
+/*
+ * Class used to keep track of the number of notifications in a <thresholdTime> period of time
+ * It is used to determine if successive notifications are part of a Burst
+ */
 
+public class BurstController {
+	
+	private ConcurrentHashMap<WatchKey, NotificationTimeStamp> notificationTimeStamps;
+	private  static long thresholdCount = 10;
+	private  static long thresholdTime = 5 * 1000; // milliseconds
+
+	/*
+	 * Default Constructor for BurstController
+	 */
 	public BurstController() {
 		this.notificationTimeStamps = new ConcurrentHashMap<WatchKey, NotificationTimeStamp>();
 	}
 
+	/*
+	 * Parameterized Constructor for BurstController
+	 */
+	public BurstController(long thresholdCount, long thresholdTime){
+		this.notificationTimeStamps = new ConcurrentHashMap<WatchKey, NotificationTimeStamp>();
+		this.thresholdCount = thresholdCount;
+		this.thresholdTime = thresholdTime;
+	}
+	
+	/*
+	 * This function returns whether or not a BURST has occurred.
+	 */
 	public String checkBurst(WatchKey key) {
+		
 		long currentTime = new Date().getTime();
-		if (notificationTimeStamps.get(key) == null) {
+		NotificationTimeStamp timeStamp = notificationTimeStamps.get(key);
+		
+		//There is no timeStamp for the current WatchKey; set to current time
+		if (timeStamp == null) {
 			notificationTimeStamps.put(key, new NotificationTimeStamp());
 			return "NO";
 		}
-		if ((currentTime - notificationTimeStamps.get(key).getSentTime() < thresholdTime)) {
-			notificationTimeStamps.get(key).incrementCount();
-			if(notificationTimeStamps.get(key).getNotificationCount() > thresholdCount){
+		
+		/*If there are more than <thresholdCount> notifications for the same path
+		 * in <thresholdTime> seconds, return "YES"
+		 * else return "NO"
+		 */
+		if ((currentTime - timeStamp.getSentTime()) < thresholdTime) {
+			timeStamp.incrementCount();
+			
+			if((notificationTimeStamps.get(key).getNotificationCount()) > thresholdCount){
 				return "YES";
 			}else{
 				return "NO";
 			}
 		} else {
-			notificationTimeStamps.put(key, new NotificationTimeStamp());
+			timeStamp.update();
 			return "NO";
 		}
 	}
