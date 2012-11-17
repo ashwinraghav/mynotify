c5053ae954aed9a03cab170d0f614220b8592cd6
Tommy Tracy II <tjt7a@virginia.edu>
2012-11-17 13:43:41 -0500

    Fixed localwatcher. It was missing the key remove logic.

--- src/watch/LocalWatcher.java
+++ src/watch/LocalWatcher.java
@@ -1,23 +1,21 @@
 package watch;
 
 import java.nio.file.*;
-import java.nio.file.WatchEvent.Kind;
 import static java.nio.file.StandardWatchEventKinds.*;
-
 import java.io.*;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 public class LocalWatcher {
 	
-		private final WatchService watcher;
-		private final Map<WatchKey, Path> keys;
-		private List<SerializableFileEvent> watchEvents;
+		private WatchService watcher;
+		private Map<WatchKey, Path> keys;
 		
 		/**
 		 * Creates a WatchService
 		 */
 		public LocalWatcher() throws IOException {
+			System.out.println("Initializing watcher");
 			this.watcher = FileSystems.getDefault().newWatchService();
 			this.keys = new HashMap<WatchKey, Path>();
 		}
@@ -27,46 +25,50 @@ public class LocalWatcher {
 		 */
 		public void register(String dirName) throws IOException {
 			Path dir = Paths.get(dirName);
-			WatchKey key = dir.register(this.watcher, ENTRY_CREATE, ENTRY_DELETE,ENTRY_MODIFY);
+			WatchKey key = dir.register(watcher, ENTRY_CREATE, ENTRY_DELETE,ENTRY_MODIFY);
 			keys.put(key, dir);
-			System.out.println("Size of key map: "+keys.size());
 		}
 
 		/**
 		 * Process all events for keys queued to the watcher
 		 */
-		public List<SerializableFileEvent> pollEvents() {
+		public ArrayList<SerializableFileEvent> pollEvents() {
 			
-			watchEvents = new ArrayList<SerializableFileEvent>();
+			ArrayList<SerializableFileEvent> watchEvents = new ArrayList<SerializableFileEvent>();
 			WatchKey key;
 			
-			long end = System.currentTimeMillis() + 2;
+			long end = System.currentTimeMillis() + 3;
 			
-			System.out.println("About to poll");
 			while(System.currentTimeMillis() < end){
-				
+				key=null;
 				try {
-					key = this.watcher.poll(1,  TimeUnit.MILLISECONDS);
+					key = this.watcher.poll(1, TimeUnit.MILLISECONDS);
 				} catch (InterruptedException x) {
-					System.err.println("Returning Null!");
 					return null;
 				}
 				if(key == null){
-					return null;
+					continue;
 				}
 				
-
 				Path dir = keys.get(key);
+				
 				if (dir == null) {
-					System.err.println("WatchKey not recognized!!");
 					continue;
 				}
 				
 				for (WatchEvent<?> event : key.pollEvents()){
 					watchEvents.add(new SerializableFileEvent(event));
 				}
+				
+				boolean valid = key.reset();
+				if (!valid) {
+					keys.remove(key);
+
+					if (keys.isEmpty()) {
+						break;
+					}
+				}
 			}
-			System.out.println("Size of watch events: "+watchEvents.size());
 			return watchEvents;
 		}
 }
