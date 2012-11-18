632d6ef92944cfa091d94673a047708eebe68014
Tommy Tracy II <tjt7a@virginia.edu>
2012-11-17 22:16:01 -0500

    Fixed; now returns jasonized messages.

--- src/watch/DistributedWatcher.java
+++ src/watch/DistributedWatcher.java
@@ -1,34 +1,39 @@
 package watch;
 
 import java.io.IOException;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchEvent.Kind;
 import java.util.ArrayList;
 import org.apache.xmlrpc.XmlRpcException;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.QueueingConsumer;
 
 public class DistributedWatcher {
-	private final RPCManager rpcManager;
-	private final ExchangeManager exchangeManager;
+	private RPCManager rpcManager;
+	private ExchangeManager exchangeManager;
 	private Channel channel;
 	private String queueName;
+	private QueueingConsumer consumer;
 	
 	/**
 	 * Creates a WatchService
 	 */
-	public DistributedWatcher() throws IOException {
+	public DistributedWatcher(boolean durable) throws IOException {
 		this.rpcManager = new RPCManager();
 		this.exchangeManager = new ExchangeManager();
 		this.channel = exchangeManager.createChannel();
-		this.queueName = channel.queueDeclare().getQueue();
+		boolean autoDelete = !durable;
+		this.queueName = channel.queueDeclare("",durable, false, autoDelete, null).getQueue();
+		this.consumer = new QueueingConsumer(channel);
+		this.channel.basicConsume(queueName, true, consumer);
 		System.out.println("Name of my queue: "+this.queueName);
 	}
 
 	/**
 	 * Register the given directory with the WatchService
 	 */
-	public int register(String dirName) throws IOException {
-			
-			Object[] parameters = new Object[] {dirName};
+	public int register(String dirName, WatchEvent.Kind<?>... subscriptionTypes) throws IOException {
+			Object[] parameters = new Object[] {new String(dirName)};
 			String i;
 			try {
 				i = (String) rpcManager.execute("HandlerClass.register",
@@ -50,7 +55,10 @@ public class DistributedWatcher {
 			exchangeManager.declareExchange(channel, dirName,
 					Constants.exchangeMap);
 			
-			channel.queueBind(queueName, dirName, "*");
+			for(Kind<?> w : subscriptionTypes){
+				channel.queueBind(this.queueName, dirName, w.name());
+			}
+			
 			System.out.println("Bound exchange: "+dirName);
 			return 0;
 	}
@@ -59,14 +67,6 @@ public class DistributedWatcher {
 	 * Process all events for keys queued to the watcher
 	 */
 	public ArrayList<SerializableFileEvent> pollEvents() {
-		
-		QueueingConsumer consumer = new QueueingConsumer(channel);
-		
-		try {
-			this.channel.basicConsume(this.queueName,true,consumer);
-		} catch (IOException e) {
-			return null;
-		}
 
 		ArrayList<SerializableFileEvent> watchEvents = new ArrayList<SerializableFileEvent>();
 		
@@ -76,8 +76,9 @@ public class DistributedWatcher {
 		while(System.currentTimeMillis() < end){
 			try {
 				//delivery = consumer.nextDelivery(1);
-				delivery = consumer.nextDelivery();
+				delivery = this.consumer.nextDelivery();
 			} catch (InterruptedException x) {
+				System.out.println("Interrupted!");
 				continue;
 			}
 			if(delivery != null)
