f395918ce9646019dd19579d2640572ae3412db0
Tommy Tracy II <tjt7a@virginia.edu>
2012-11-20 23:48:28 -0500

    Cleaned up code; removed loop

--- src/watch/LocalWatcher.java
+++ src/watch/LocalWatcher.java
@@ -7,68 +7,65 @@ import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 public class LocalWatcher {
-	
-		private WatchService watcher;
-		private Map<WatchKey, Path> keys;
-		
-		/**
-		 * Creates a WatchService
-		 */
-		public LocalWatcher() throws IOException {
-			System.out.println("Initializing watcher");
-			this.watcher = FileSystems.getDefault().newWatchService();
-			this.keys = new HashMap<WatchKey, Path>();
+
+	private WatchService watcher;
+	private Map<WatchKey, Path> keys;
+
+	/**
+	 * Creates a WatchService
+	 * @throws IOException
+	 */
+	public LocalWatcher() throws IOException {
+		this.watcher = FileSystems.getDefault().newWatchService();
+		this.keys = new HashMap<WatchKey, Path>();
+	}
+
+	/**
+	 * Register the given directory with the WatchService
+	 * @param dirName
+	 * @throws IOException
+	 */
+	public void register(String dirName) throws IOException {
+		Path dir = Paths.get(dirName);
+		WatchKey key = dir.register(watcher, ENTRY_CREATE, ENTRY_DELETE,ENTRY_MODIFY);
+		keys.put(key, dir);
+	}
+
+	/**
+	 * Process all events for keys queued to the watcher
+	 */
+	public ArrayList<SerializableFileEvent> pollEvents(){
+
+		ArrayList<SerializableFileEvent> watchEvents = new ArrayList<SerializableFileEvent>();
+		WatchKey key = null;
+
+		try {
+			key = this.watcher.poll(1, TimeUnit.MILLISECONDS);
+		} catch (InterruptedException x) {
+			return null;
+		}
+		if(key == null){
+			return null;
 		}
 
-		/**
-		 * Register the given directory with the WatchService
-		 */
-		public void register(String dirName) throws IOException {
-			Path dir = Paths.get(dirName);
-			WatchKey key = dir.register(watcher, ENTRY_CREATE, ENTRY_DELETE,ENTRY_MODIFY);
-			keys.put(key, dir);
+		Path dir = keys.get(key);
+
+		if (dir == null) {
+			return null;
 		}
 
-		/**
-		 * Process all events for keys queued to the watcher
-		 */
-		public ArrayList<SerializableFileEvent> pollEvents() {
-			
-			ArrayList<SerializableFileEvent> watchEvents = new ArrayList<SerializableFileEvent>();
-			WatchKey key;
-			
-			long end = System.currentTimeMillis() + 3;
-			
-			while(System.currentTimeMillis() < end){
-				key=null;
-				try {
-					key = this.watcher.poll(1, TimeUnit.MILLISECONDS);
-				} catch (InterruptedException x) {
-					return null;
-				}
-				if(key == null){
-					continue;
-				}
-				
-				Path dir = keys.get(key);
-				
-				if (dir == null) {
-					continue;
-				}
-				
-				for (WatchEvent<?> event : key.pollEvents()){
-					watchEvents.add(new SerializableFileEvent(event, dir));
-				}
-				
-				boolean valid = key.reset();
-				if (!valid) {
-					keys.remove(key);
+		for (WatchEvent<?> event : key.pollEvents()){
+			watchEvents.add(new SerializableFileEvent(event, dir));
+		}
 
-					if (keys.isEmpty()) {
-						break;
-					}
-				}
+		boolean valid = key.reset();
+		if (!valid) {
+			keys.remove(key);
+
+			if (keys.isEmpty()) {
+				return null;
 			}
-			return watchEvents;
 		}
+		return watchEvents;
+	}
 }
