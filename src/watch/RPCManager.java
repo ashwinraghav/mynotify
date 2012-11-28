338910da3bcf743bf3dcfdb90939f86d40371781
Tommy Tracy II <tjt7a@virginia.edu>
2012-11-28 18:31:56 -0500

    Added comments

--- src/watch/RPCManager.java
+++ src/watch/RPCManager.java
@@ -7,23 +7,35 @@ import org.apache.xmlrpc.XmlRpcException;
 import org.apache.xmlrpc.client.XmlRpcClient;
 import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
 
+
 public class RPCManager {
 	XmlRpcClient client;
+	
+	/**
+	 * Return RPC Client bound to passed hostname
+	 * @param serverHostName
+	 * @throws MalformedURLException
+	 */
 	public RPCManager(String serverHostName) throws MalformedURLException {
 		client = getRPCClient(serverHostName);
 	}
-	
-	public static XmlRpcClient getRPCClient(String serverHostName) throws MalformedURLException {
-		XmlRpcClientConfigImpl config;
-		XmlRpcClient client;
-		config = new XmlRpcClientConfigImpl();
-		System.out.println(serverHostName);
-		config.setServerURL(new URL(serverHostName));
 
-		client = new XmlRpcClient();
+	//Create new XmlRpcClient and set hostname to passed serverHostName value
+	private static XmlRpcClient getRPCClient(String serverHostName) throws MalformedURLException {
+		XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
+		config.setServerURL(new URL(serverHostName));
+		XmlRpcClient client = new XmlRpcClient();
 		client.setConfig(config);
 		return client;
 	}
+	
+	/**
+	 * Execute the methodName with the given parameters synchronously and return String value
+	 * @param methodName
+	 * @param parameters
+	 * @return
+	 * @throws XmlRpcException
+	 */
 	public String execute(String methodName, Object[] parameters) throws XmlRpcException{
 		return (String)client.execute(methodName, parameters);
 	}
