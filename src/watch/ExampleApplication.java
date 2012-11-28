cc512c872280eeab06e0c180e5ed09e9112a21b9
Tommy Tracy II <tjt7a@virginia.edu>
2012-11-28 18:11:44 -0500

    Cleaned up code and added comments

--- src/watch/ExampleApplication.java
+++ src/watch/ExampleApplication.java
@@ -1,29 +1,29 @@
 package watch;
-
-import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
-import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
-import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
-
 import java.util.ArrayList;
 
 public class ExampleApplication {
 
 	public static void main(String[] args) throws InterruptedException {
+		
+		//Directory to be subscribed to
 		String directory = "/localtmp/dump";
 
 		// Allow user to pass name of directory as command line argument
 		if (args.length == 1)
 			directory = args[0];
 
+		//Constract MyFSWatcher
 		MyFSWatcher cs = new MyFSWatcher();
+		
+		//Subscribe to directory and 4 additional subdirectories
 		cs.subscribe(directory);
 		cs.subscribe(directory + "/1");
 		cs.subscribe(directory + "/2");
 		cs.subscribe(directory + "/3");
 		cs.subscribe(directory + "/4");
 
+		//Continunously poll the watcher for new notifications and handle them
 		ArrayList<SerializableFileEvent> events;
-
 		while (true) {
 			events = cs.pollEvent();
 			for (int i = 0; i < events.size(); i++) {
