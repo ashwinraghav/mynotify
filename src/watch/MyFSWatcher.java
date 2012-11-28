b826c071e1a53e2a799ee754af96b642e3199dd7
Tommy Tracy II <tjt7a@virginia.edu>
2012-11-28 18:27:18 -0500

    Added comments as well as debug flag (for verbose output), and testDistributed flag to force all registrations to be done via the server.

--- src/watch/MyFSWatcher.java
+++ src/watch/MyFSWatcher.java
@@ -19,10 +19,12 @@ import java.util.Map;
  */
 public class MyFSWatcher {
 
-	// ArrayList of NFS mountpoints
 	private HashMap<String, String> mounted;
 	private LocalWatcher lw;
 	private DistributedWatcher dw;
+	private final int port = 8080;
+	private boolean testingDistributed = true;
+	private boolean debug = true;
 
 	/**
 	 * Default Subscriber Constructor
@@ -42,6 +44,7 @@ public class MyFSWatcher {
 		String os = System.getProperty("os.name").toLowerCase();
 		String searchString = null;
 
+		
 		if (os.contains("mac")) {
 			searchString = "(nfs";
 		} else if ((os.contains("nix") || (os.contains("linux")))) {
@@ -51,6 +54,9 @@ public class MyFSWatcher {
 			return;
 		}
 
+		if(debug)
+			System.out.println("Searching local OS for NFS directories...");
+		
 		String line;
 		try {
 			Runtime r = Runtime.getRuntime();
@@ -70,11 +76,13 @@ public class MyFSWatcher {
 							.asList(mount_info.get(i).split(" ")));
 
 					String hostName = splitLine.get(0).split(":")[0];
-					System.out.println("Hostname: " + hostName);
 
 					int indexOfPath = splitLine.indexOf("on");
 					String mountPoint = splitLine.get(++indexOfPath);
-					mounted.put(mountPoint, hostName);
+					//Format hostname and add port
+					if(debug)
+						System.out.println("Adding NFS entry; Hostname: "+hostName+", Mountpoint:"+mountPoint);
+					mounted.put(mountPoint, "http://"+hostName+":"+port);
 				}
 			}
 		} catch (Exception e) {
@@ -85,38 +93,45 @@ public class MyFSWatcher {
 	}
 
 	/**
-	 * 
+	 * Subscribe to a directory
 	 * @param dirName
 	 * @return 0 if local file is registered; 1 if distributed file is
 	 *         registered Doesn't actually return though
 	 */
 	public int subscribe(String dirName) {
+		
 		boolean durable = false;
-
-		Iterator<Map.Entry<String, String>> iter = mounted.entrySet()
-				.iterator();
-		String tempdir;
+		Iterator<Map.Entry<String, String>> iter = mounted.entrySet().iterator();
 		String temphost;
+		
+		if(testingDistributed){
+			//Subscribe to nfs directory; default to all notification types
+			nfssubscribe(Constants.serverUrl, dirName, durable, ENTRY_CREATE,
+					ENTRY_DELETE, ENTRY_MODIFY,
+					NotificationStopEvent.NOTIFICATION_STOP);
+			return 1;
+		}
 
 		while (iter.hasNext()) {
-			Map.Entry pairs = iter.next();
+			Map.Entry<String,String> pairs = iter.next();
 			if (dirName.startsWith((String) pairs.getKey())) {
-				tempdir = (String) pairs.getKey();
 				temphost = (String) pairs.getValue();
-				System.out.println("NFS subscription for: " + pairs.getValue());
+				if(debug)
+					System.out.println("NFS subscription for: " + temphost);
+				//Subscribe to nfs directory; default to all notification types
 				nfssubscribe(temphost, dirName, durable, ENTRY_CREATE,
 						ENTRY_DELETE, ENTRY_MODIFY,
 						NotificationStopEvent.NOTIFICATION_STOP);
 				return 1;
 			}
 		}
-
-		// System.out.println("Local Directory Subscription for: " + dirName);
-		// localsubscribe(dirName, durable, ENTRY_CREATE,
-		// ENTRY_DELETE,ENTRY_MODIFY, NotificationStopEvent.NOTIFICATION_STOP);
-		nfssubscribe(Constants.serverUrl, dirName, durable, ENTRY_CREATE,
-				ENTRY_DELETE, ENTRY_MODIFY,
-				NotificationStopEvent.NOTIFICATION_STOP);
+		
+		//If not distributed, use local
+		//Subscribe to local directory; default to all notification types
+		if(debug)
+			System.out.println("Local subscription for: "+dirName);
+		
+		localsubscribe(dirName, durable, ENTRY_CREATE,ENTRY_DELETE,ENTRY_MODIFY, NotificationStopEvent.NOTIFICATION_STOP);
 		return 0;
 	}
 
@@ -145,46 +160,51 @@ public class MyFSWatcher {
 	/*
 	 * Method for subscribing to a directory that is local
 	 */
-	private int localsubscribe(String dirName, boolean durable,
-			WatchEvent.Kind<?>... subscriptionTypes) {
+	private int localsubscribe(String dirName, boolean durable, WatchEvent.Kind<?>... subscriptionTypes) {
+		
+		//If a local watcher has not been instantiated yet, create one
 		if (this.lw == null) {
 			try {
 				this.lw = new LocalWatcher();
-				System.out.println("Created local watcher");
 			} catch (IOException e) {
 				e.printStackTrace();
 				return -1;
 			}
 		}
+		//Register the passed directory name
 		try {
-			this.lw.register(dirName);
-			System.out.println("Finished registering: " + dirName);
+			this.lw.register(dirName, subscriptionTypes);
 		} catch (IOException e) {
-			System.err.println("Could not register; " + dirName);
+			System.err.println("Could not locally register: " + dirName);
 			return -1;
 		}
 		return 0;
 	}
 
-	public ArrayList<SerializableFileEvent> pollEvent()
-			throws InterruptedException {
+	/**
+	 * Poll for events
+	 * @return ArrayList of events
+	 * @throws InterruptedException
+	 */
+	public ArrayList<SerializableFileEvent> pollEvent() throws InterruptedException {
+		
 		ArrayList<SerializableFileEvent> temp;
 		ArrayList<SerializableFileEvent> messages = new ArrayList<SerializableFileEvent>();
 
+		//Poll distributed watcher first
 		if (dw != null) {
 			temp = dw.pollEvents();
 			if (temp != null) {
 				messages.addAll(temp);
 			}
-
 		}
+		//Then poll local watcher
 		if (lw != null) {
 			temp = lw.pollEvents();
 			if (temp != null) {
 				messages.addAll(temp);
 			}
 		}
-
 		return messages;
 	}
 }
